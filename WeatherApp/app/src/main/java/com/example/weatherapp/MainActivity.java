a701db68940ed3a0aa1c5241b7e2ce93dcfb4c56
YueLi1996 <58449011+YueLi1996@users.noreply.github.com>
2021-11-16 23:10:16 -0800

    add tableView in card_item.xml then figure out usage of viewpager in activity_home.xml

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,14 +2,39 @@ package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // set theme back to normal after splash screen
         setTheme(R.style.Theme_WeatherApp);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView fetchWeatherView = (TextView) findViewById(R.id.fetchWeatherText);
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        // Display loading page for 3 sec
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                //Log.i("info", "jin lai le");
+                fetchWeatherView.setVisibility(View.GONE);
+                progressBar.setVisibility(View.GONE);
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        }, 3000);
     }
 }
