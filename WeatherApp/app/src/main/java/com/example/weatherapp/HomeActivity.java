49088e196251711680e0911714a96eb799a3e883
YueLi1996 <58449011+YueLi1996@users.noreply.github.com>
2021-11-30 00:42:43 -0800

    get two APIs workable and need to update info at HOMEACTIVITY

--- WeatherApp/app/src/main/java/com/example/weatherapp/HomeActivity.java
+++ WeatherApp/app/src/main/java/com/example/weatherapp/HomeActivity.java
@@ -96,7 +96,7 @@ public class HomeActivity extends AppCompatActivity {
                                         @Override
                                         public void onResponse(JSONObject response) {
                                             try {
-//                                                System.out.println(tmrURL);
+                                                System.out.println(tmrURL);
                                                 ImageView weatherIcon = (ImageView) findViewById(R.id.weatherIcon);
                                                 TextView temperature = (TextView) findViewById(R.id.temperature);
                                                 TextView weatherType = (TextView) findViewById(R.id.weatherType);
@@ -104,40 +104,82 @@ public class HomeActivity extends AppCompatActivity {
                                                 TextView windSpeedData = (TextView) findViewById(R.id.windSpeedData);
                                                 TextView visibilityData = (TextView) findViewById(R.id.visibilityData);
                                                 TextView pressureData = (TextView) findViewById(R.id.pressureData);
-                                                TextView date1 = (TextView) findViewById(R.id.date1);
-                                                ImageView weatherRep1 = (ImageView) findViewById(R.id.weatherRep1);
-                                                TextView minTemperature1 = (TextView) findViewById(R.id.minTemperature1);
-                                                TextView maxTemperature1 = (TextView) findViewById(R.id.maxTemperature1);
-                                                TextView date2 = (TextView) findViewById(R.id.date2);
-                                                ImageView weatherRep2 = (ImageView) findViewById(R.id.weatherRep2);
-                                                TextView minTemperature2 = (TextView) findViewById(R.id.minTemperature2);
-                                                TextView maxTemperature2 = (TextView) findViewById(R.id.maxTemperature2);
-                                                TextView date3 = (TextView) findViewById(R.id.date3);
-                                                ImageView weatherRep3 = (ImageView) findViewById(R.id.weatherRep3);
-                                                TextView minTemperature3 = (TextView) findViewById(R.id.minTemperature3);
-                                                TextView maxTemperature3 = (TextView) findViewById(R.id.maxTemperature3);
-                                                TextView date4 = (TextView) findViewById(R.id.date4);
-                                                ImageView weatherRep4 = (ImageView) findViewById(R.id.weatherRep4);
-                                                TextView minTemperature4 = (TextView) findViewById(R.id.minTemperature4);
-                                                TextView maxTemperature4 = (TextView) findViewById(R.id.maxTemperature4);
-                                                TextView date5 = (TextView) findViewById(R.id.date5);
-                                                ImageView weatherRep5 = (ImageView) findViewById(R.id.weatherRep5);
-                                                TextView minTemperature5 = (TextView) findViewById(R.id.minTemperature5);
-                                                TextView maxTemperature5 = (TextView) findViewById(R.id.maxTemperature5);
-                                                TextView date6 = (TextView) findViewById(R.id.date6);
-                                                ImageView weatherRep6 = (ImageView) findViewById(R.id.weatherRep6);
-                                                TextView minTemperature6 = (TextView) findViewById(R.id.minTemperature6);
-                                                TextView maxTemperature6 = (TextView) findViewById(R.id.maxTemperature6);
 
                                                 JSONObject data = response.getJSONObject("data");
                                                 JSONArray timelines = data.getJSONArray("timelines");
                                                 JSONObject element = (JSONObject) timelines.get(0);
                                                 JSONArray intervals = element.getJSONArray("intervals");
-                                                JSONObject oneDay = (JSONObject) intervals.get(0);
-                                                JSONObject values = oneDay.getJSONObject("values");
-                                                //String haha = Integer.toString((int) Math.round(values.getDouble("temperature")));
-                                                temperature.setText(Integer.toString((int) Math.round(values.getDouble("temperature")))+" \u2109");
+                                                JSONObject today = (JSONObject) intervals.get(0);
+                                                JSONObject values = today.getJSONObject("values");
 
+                                                weatherType.setText(getWeatherInfo(values.getString("weatherCode")));
+                                                temperature.setText(Integer.toString((int) Math.round(values.getDouble("temperature")))+" \u2109");
+//                                                System.out.println(values.getString("weatherCode"));
+
+                                                humidtyData.setText(values.getString("humidity") + "%");
+                                                windSpeedData.setText(values.getString("windSpeed") + "mph");
+                                                visibilityData.setText(values.getString("visibility") + "mi");
+                                                pressureData.setText(values.getString("pressureSurfaceLevel") + "inHg");
+
+                                                JSONObject day1 = (JSONObject) intervals.get(1);
+                                                JSONObject values1 = day1.getJSONObject("values");
+                                                TextView date1 = (TextView) findViewById(R.id.date1);
+                                                ImageView weatherRep1 = (ImageView) findViewById(R.id.weatherRep1);
+                                                TextView minTemperature1 = (TextView) findViewById(R.id.minTemperature1);
+                                                TextView maxTemperature1 = (TextView) findViewById(R.id.maxTemperature1);
+                                                date1.setText(day1.getString("startTime").split("T",0)[0]);
+                                                minTemperature1.setText(Integer.toString((int) Math.round(values1.getDouble("temperatureMin"))));
+                                                maxTemperature1.setText(Integer.toString((int) Math.round(values1.getDouble("temperatureMax"))));
+
+                                                JSONObject day2 = (JSONObject) intervals.get(2);
+                                                JSONObject values2 = day2.getJSONObject("values");
+                                                TextView date2 = (TextView) findViewById(R.id.date2);
+                                                ImageView weatherRep2 = (ImageView) findViewById(R.id.weatherRep2);
+                                                TextView minTemperature2 = (TextView) findViewById(R.id.minTemperature2);
+                                                TextView maxTemperature2 = (TextView) findViewById(R.id.maxTemperature2);
+                                                date2.setText(day2.getString("startTime").split("T",0)[0]);
+                                                minTemperature2.setText(Integer.toString((int) Math.round(values2.getDouble("temperatureMin"))));
+                                                maxTemperature2.setText(Integer.toString((int) Math.round(values2.getDouble("temperatureMax"))));
+
+                                                JSONObject day3 = (JSONObject) intervals.get(3);
+                                                JSONObject values3 = day3.getJSONObject("values");
+                                                TextView date3 = (TextView) findViewById(R.id.date3);
+                                                ImageView weatherRep3 = (ImageView) findViewById(R.id.weatherRep3);
+                                                TextView minTemperature3 = (TextView) findViewById(R.id.minTemperature3);
+                                                TextView maxTemperature3 = (TextView) findViewById(R.id.maxTemperature3);
+                                                date3.setText(day3.getString("startTime").split("T",0)[0]);
+                                                minTemperature3.setText(Integer.toString((int) Math.round(values3.getDouble("temperatureMin"))));
+                                                maxTemperature3.setText(Integer.toString((int) Math.round(values3.getDouble("temperatureMax"))));
+
+                                                JSONObject day4 = (JSONObject) intervals.get(4);
+                                                JSONObject values4 = day4.getJSONObject("values");
+                                                TextView date4 = (TextView) findViewById(R.id.date4);
+                                                ImageView weatherRep4 = (ImageView) findViewById(R.id.weatherRep4);
+                                                TextView minTemperature4 = (TextView) findViewById(R.id.minTemperature4);
+                                                TextView maxTemperature4 = (TextView) findViewById(R.id.maxTemperature4);
+                                                date4.setText(day4.getString("startTime").split("T",0)[0]);
+                                                minTemperature4.setText(Integer.toString((int) Math.round(values4.getDouble("temperatureMin"))));
+                                                maxTemperature4.setText(Integer.toString((int) Math.round(values4.getDouble("temperatureMax"))));
+
+                                                JSONObject day5 = (JSONObject) intervals.get(5);
+                                                JSONObject values5 = day5.getJSONObject("values");
+                                                TextView date5 = (TextView) findViewById(R.id.date5);
+                                                ImageView weatherRep5 = (ImageView) findViewById(R.id.weatherRep5);
+                                                TextView minTemperature5 = (TextView) findViewById(R.id.minTemperature5);
+                                                TextView maxTemperature5 = (TextView) findViewById(R.id.maxTemperature5);
+                                                date5.setText(day5.getString("startTime").split("T",0)[0]);
+                                                minTemperature5.setText(Integer.toString((int) Math.round(values5.getDouble("temperatureMin"))));
+                                                maxTemperature5.setText(Integer.toString((int) Math.round(values5.getDouble("temperatureMax"))));
+
+                                                JSONObject day6 = (JSONObject) intervals.get(6);
+                                                JSONObject values6 = day6.getJSONObject("values");
+                                                TextView date6 = (TextView) findViewById(R.id.date6);
+                                                ImageView weatherRep6 = (ImageView) findViewById(R.id.weatherRep6);
+                                                TextView minTemperature6 = (TextView) findViewById(R.id.minTemperature6);
+                                                TextView maxTemperature6 = (TextView) findViewById(R.id.maxTemperature6);
+                                                date6.setText(day6.getString("startTime").split("T",0)[0]);
+                                                minTemperature6.setText(Integer.toString((int) Math.round(values6.getDouble("temperatureMin"))));
+                                                maxTemperature6.setText(Integer.toString((int) Math.round(values6.getDouble("temperatureMax"))));
 
                                             } catch (JSONException e) {
                                                 e.printStackTrace();
@@ -170,6 +212,102 @@ public class HomeActivity extends AppCompatActivity {
         // Add the request to the RequestQueue.
         queue.add(geoJsonObjectRequest);
 
+    }
+
+    private String getWeatherInfo(String code_number) {
+        if(code_number.equals("1000"))
+        {
+            return "Clear";
+        }
+        if(code_number.equals("1100"))
+        {
+            return "Mostly Clear";
+        }
+        if(code_number.equals("1101"))
+        {
+            return "Partly Cloudy";
+        }
+        if(code_number.equals("1102"))
+        {
+            return "Mostly Cloudy";
+        }
+        if(code_number.equals("1001"))
+        {
+            return "Cloudy";
+        }
+        if(code_number == "2000")
+        {
+            return "Fog";
+        }
+        if(code_number == "2100")
+        {
+            return "Light Fog";
+        }
+        if(code_number == "8000")
+        {
+            return "   Thunderstorm";
+        }
+        if(code_number == "5001")
+        {
+            return "Flurries";
+        }
+        if(code_number == "5100")
+        {
+            return "Light Snow";
+        }
+        if(code_number == "5000")
+        {
+            return "Snow";
+        }
+        if(code_number == "5101")
+        {
+            return "Heavy Snow";
+        }
+        if(code_number == "7102")
+        {
+            return "Light Ice Pellets";
+        }
+        if(code_number == "7000")
+        {
+            return "Ice Pellets";
+        }
+        if(code_number == "7101")
+        {
+            return "Heavy Ice Pellets";
+        }
+        if(code_number == "4000")
+        {
+            return "Drizzle";
+        }
+        if(code_number == "6000")
+        {
+            return "Freezing Drizzle";
+        }
+        if(code_number == "6200")
+        {
+            return "Light Freezing Rain";
+        }
+        if(code_number == "6001")
+        {
+            return "Freezing Rain";
+        }
+        if(code_number == "6201")
+        {
+            return "Heavy Freezing Rain";
+        }
+        if(code_number == "4200")
+        {
+            return "Light Rain";
+        }
+        if(code_number == "4001")
+        {
+            return "Rain";
+        }
+        if(code_number == "4201")
+        {
+            return "Heavy Rain";
+        }
+        return "null";
     }
 
     @Override
